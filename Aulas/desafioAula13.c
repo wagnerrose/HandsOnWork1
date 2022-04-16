bc303c4b617a1c1c1706785da267a99da50a1e41
Wagner Rose <wagner.rose@gmail.com>
2022-04-16 11:51:31 -0300

    atualizacao inicial aulas de estudo

--- Aulas/desafioAula13.c
+++ Aulas/desafioAula13.c
@@ -8,16 +8,16 @@ void imprimeVoto(int *valores, int total);
 int main()
 {
     int voto;
-    char fim = 'N';
+    char fim;
     int totalVotos = 0;
     int contagem[ITENS] = {0,0,0,0,0,0};
     for(;;)
     {
         voto = leVoto();
         ++contagem[voto-1];
-        totalVotos= totalVotos + 1;
+        ++totalVotos;
         printf("\nVota de novo? Para terminar digite N ou n.\n");
-        scanf(" %c", &fim);
+        scanf("%c", &fim);
         if (fim == 'n' || fim == 'N') break;
     }
     imprimeVoto(contagem, totalVotos);
@@ -36,17 +36,17 @@ int leVoto()
     printf("5. Mac OS.\n");
     printf("6. Outros.\n");
     printf("Voto: ");
-    scanf("%d\n",&escolha);
+    scanf("%d",&escolha);
     return escolha;
 }
 
 void imprimeVoto(int *valores, int total)
 {
     printf("\nTotalizacao de Votos\n");
-    printf("1. Windows Server --> %d %f\n",valores[0], valores[0]/total);
-    printf("2. Unix --> %d %f\n",valores[1], valores[1]/total);
-    printf("3. Linux --> %d %f\n",valores[2], valores[2]/total);
-    printf("4. Netware --> %d %f\n",valores[3], valores[3]/total);
-    printf("5. Mac OS --> %d %f\n",valores[4], valores[4]/total);
-    printf("6. Outros --> %d %f\n",valores[5], valores[5]/total);
+    printf("1. Windows Server --> %d - %.2f\%\n",valores[0], ((float) valores[0]/total)*100);
+    printf("2. Unix           --> %d - %.2f\%\n",valores[1], ((float) valores[1]/total)*100);
+    printf("3. Linux          --> %d - %.2f\%\n",valores[2], ((float) valores[2]/total)*100);
+    printf("4. Netware        --> %d - %.2f\%\n",valores[3], ((float) valores[3]/total)*100);
+    printf("5. Mac OS         --> %d - %.2f\%\n",valores[4], ((float) valores[4]/total)*100);
+    printf("6. Outros         --> %d - %.2f\%\n",valores[5], ((float) valores[5]/total)*100);
  }
